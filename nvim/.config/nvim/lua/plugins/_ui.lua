9eaf49dfecea35cc4648f8dc2451f417b7e7f380
xthirtysix <ggalliosha@gmail.com>
2025-01-12 09:01:48 +0300

    🧹 chore(nvim): remove comments and unused configs from theme file

--- nvim/.config/nvim/lua/plugins/_ui.lua
+++ nvim/.config/nvim/lua/plugins/_ui.lua
@@ -5,14 +5,13 @@ return {
         name = 'rose-pine',
         config = function()
             require('rose-pine').setup {
-                variant = 'auto', -- auto, main, moon, or dawn
+                variant = 'main', -- auto, main, moon, or dawn
                 dark_variant = 'main', -- main, moon, or dawn
                 dim_inactive_windows = false,
                 extend_background_behind_borders = true,
 
                 enable = {
                     terminal = true,
-                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
                     migrations = true, -- Handle deprecated options automatically
                 },
 
@@ -52,37 +51,9 @@ return {
                     h5 = 'pine',
                     h6 = 'foam',
                 },
-
-                palette = {
-                    -- Override the builtin palette per variant
-                    -- moon = {
-                    --     base = '#18191a',
-                    --     overlay = '#363738',
-                    -- },
-                },
-
-                highlight_groups = {
-                    -- Comment = { fg = "foam" },
-                    -- VertSplit = { fg = "muted", bg = "muted" },
-                },
-
-                before_highlight = function(_group, highlight, palette)
-                    -- Disable all undercurls
-                    if highlight.undercurl then
-                        highlight.undercurl = false
-                    end
-
-                    -- Change palette colour
-                    if highlight.fg == palette.pine then
-                        highlight.fg = palette.foam
-                    end
-                end,
             }
 
             vim.cmd 'colorscheme rose-pine'
-            -- vim.cmd("colorscheme rose-pine-main")
-            -- vim.cmd("colorscheme rose-pine-moon")
-            -- vim.cmd("colorscheme rose-pine-dawn")
         end,
     },
 }
