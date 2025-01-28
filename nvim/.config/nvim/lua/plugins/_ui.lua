a24023ed8e6adb6cdb50c541ea63a0603167db38
xthirtysix <ggalliosha@gmail.com>
2025-01-28 20:55:43 +0300

    ✨ feat(nvim): install noice plugin

--- nvim/.config/nvim/lua/plugins/_ui.lua
+++ nvim/.config/nvim/lua/plugins/_ui.lua
@@ -51,9 +51,60 @@ return {
                     h5 = 'pine',
                     h6 = 'foam',
                 },
+
+                highlight_groups = {
+                    Whitespace = { fg = 'highlight_med' },
+                    VertSplit = { fg = 'muted', bg = 'muted' },
+                },
             }
 
             vim.cmd 'colorscheme rose-pine'
         end,
+    },
+    -- popup, cmdline, messages UI replacement
+    {
+        'folke/noice.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+            'rcarriga/nvim-notify',
+        },
+        config = function()
+            require('telescope').load_extension 'noice'
+            require('notify').setup {
+                background_colour = '#1f1d2e',
+            }
+            require('noice').setup {
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                        ['vim.lsp.util.stylize_markdown'] = true,
+                        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            }
+        end,
+    },
+    -- smooth scrolling
+    {
+        'declancm/cinnamon.nvim',
+        version = '*', -- use latest release
+        opts = {
+            -- change default options here
+            keymaps = {
+                basic = true,
+                extra = true,
+            },
+            options = { mode = 'window' },
+        },
     },
 }
